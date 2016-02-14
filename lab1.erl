795084547e47dcf90138e5f298e17d9d4fb4dde2
Denis Volobuev <deevolt@yandex.ru>
2016-02-14 16:55:59 +0500

    cost function added

---
+++ lab1.erl
@@ -0,0 +1,24 @@
+-module(lab1).
+-export([fac/1, fib/1, member/2, cost/1]).
+
+fac(0)->1;
+fac(1)->1;
+fac(N)->N * fac(N - 1).
+
+fib(1)->0;
+fib(2)->1;
+fib(N)->fib(N - 1) + fib( N - 2).
+
+member([], _)->false;
+member([H|_], H)->true;
+member([H|T], M)->member(T, M).
+
+price(apple)->50;
+price(milk)->60;
+price(bread)->30;
+price(beef)->300;
+price(pork)->250.
+
+cost(L)->cost(L, 0).
+cost([], M)->M;
+cost([{G, C}|T], M)->cost(T, M + price(G) * C).
